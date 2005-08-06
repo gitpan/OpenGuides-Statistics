47af67ca91742a96b62fb8f56ae083d8c33dbc0a
EMARTIN <emartin@cpan.org>
2005-08-06 22:04:41 +0000

    Import of EMARTIN/OpenGuides-Statistics-1.2 from CPAN.
    gitpan-cpan-distribution: OpenGuides-Statistics
    gitpan-cpan-version:      1.2
    gitpan-cpan-path:         EMARTIN/OpenGuides-Statistics-1.2.tar.gz
    gitpan-cpan-author:       EMARTIN
    gitpan-cpan-maturity:     released

--- lib/OpenGuides/Statistics.pm
+++ lib/OpenGuides/Statistics.pm
@@ -5,7 +5,7 @@ use strict;
 
 use vars qw($VERSION);
 
-$VERSION = 1.1;
+$VERSION = 1.2;
 
 use Chart::Strip;
 use DBI;
@@ -19,7 +19,7 @@ sub new
   my %options = @_;
   my $self  = bless {}, $class;
 
-  die "No wiki configuration file specified"  unless $options{wiki_conf};
+  die "Error: No wiki configuration file specified"  unless $options{wiki_conf};
 
   stat $options{wiki_conf} or die "Couldn't open wiki configuration file ($options{wiki_conf}): $!" if $!;
 
@@ -29,7 +29,7 @@ sub new
   {
     if ($options{$_})
     {
-      die "Graph height and width must be numbers" unless looks_like_number($options{$_}) == 1;
+      die "Error: '$_' must be a numbers; you specified '$_'." unless looks_like_number($options{$_});
       $self->$_($options{$_});
     }
   }
@@ -38,101 +38,137 @@ sub new
   {
     if ($options{$_})
     {
-      die "Graph colours must be 6-digit hex values" unless $options{$_} =~ /^[0-9a-fA-F]{6}$/;
+      die "Error: '$_' must be a 6-digit hex value; you specified '$options{$_}'." unless $options{$_} =~ /^[0-9a-fA-F]{6}$/;
       $self->$_($options{$_}); 
     }
   }
 
   if ($options{import_date})
   {
-    die "Error: import date ($options{import_date}) was not in yyyy-mm-dd format" unless $options{import_date} =~ /^\d\d\d\d-\d\d-\d\d$/;
+    die "Error: 'import_date' must be in yyyy-mm-dd format; you specified '$options{import_date}'." unless $options{import_date} =~ /^\d\d\d\d-\d\d-\d\d$/;
     $self->import_date($options{import_date});
   }
   
   if ($options{include_redirects})
   {
-    die "Error: include_redirects must be set to 1 or 0" unless $options{include_redirects} =~ /^[0-1]$/;
+    die "Error: 'include_redirects' must be set to '1' if you set it." unless $options{include_redirects} eq "1";
     $self->include_redirects($options{include_redirects});
-  }
-  
-  if ($options{error_threshold})
-  {
-    die "Error: error_threshold must be a number" unless looks_like_number($options{include_redirects}) == 1;
-    $self->error_threshold($options{error_threshold});
   }
   
   $self;
 }
 
+sub dbh
+{
+  my $self = shift;
+
+  unless ($self->{dbh})
+  {
+    my $dsn;
+
+    my $wiki_conf = $self->wiki_conf;
+    my $dbtype    = $wiki_conf->dbtype;
+    my $dbname    = $wiki_conf->dbname;
+    my $dbhost    = $wiki_conf->dbhost || '';
+    my $dbuser    = $wiki_conf->dbuser || '';
+    my $dbpass    = $wiki_conf->dbpass || '';
+
+    if    ($dbtype eq 'mysql')    { $dsn = "dbi:mysql:database=$dbname;host=$dbhost"; }
+    elsif ($dbtype eq 'postgres') { $dsn = "dbi:Pg:dbname=$dbname;host=$dbhost"; }
+    elsif ($dbtype eq 'sqlite')   { $dsn = "dbi:SQLite:dbname=$dbname"; }
+    else                          { die "Unknown database type specified: $dbtype"; }
+
+    $self->{dbh} = DBI->connect($dsn, $dbuser, $dbpass) or die "Couldn't connect to database: $!";
+  }
+  
+  $self->{dbh};
+}
+
 sub retrieve_node_data
 {
   my $self = shift;
-  my $dsn;
 
-  my $wiki_conf = $self->wiki_conf;
-  my $dbtype    = $wiki_conf->dbtype;
-  my $dbname    = $wiki_conf->dbname;
-  my $dbhost    = $wiki_conf->dbhost || '';
-  my $dbuser    = $wiki_conf->dbuser || '';
-  my $dbpass    = $wiki_conf->dbpass || '';
+  my $dbh = $self->dbh;
 
-  if    ($dbtype eq 'mysql')    { $dsn = "dbi:mysql:database=$dbname;host=$dbhost"; }
-  elsif ($dbtype eq 'postgres') { $dsn = "dbi:Pg:dbname=$dbname;host=$dbhost"; }
-  elsif ($dbtype eq 'sqlite')   { $dsn = "dbi:SQLite:dbname=$dbname"; }
-  else                          { die "Unknown database type specified: $dbtype"; }
+  my $get_node_names = "SELECT DISTINCT name FROM node";
 
-  my $dbh = DBI->connect($dsn, $dbuser, $dbpass) or die "Couldn't connect to database: $!";
-
-  my $get_nodes = "SELECT name, modified FROM content WHERE version='1'";
-
-  my $sth = $dbh->prepare($get_nodes);
-  $sth->execute or die $get_nodes;
-
-  my %nodes;
-
-  while (my @node_data = $sth->fetchrow_array)
+  my $sth = $dbh->prepare($get_node_names);
+  $sth->execute or die $get_node_names;
+  
+  my (@list_of_nodes, %nodes);
+  
+  while (my @node_names = $sth->fetchrow_array)
   {
-    my $name = $node_data[0];
+    push @list_of_nodes, $node_names[0];
+  }  
+
+  my $node_count = 0;
+
+  foreach (@list_of_nodes)
+  {
+    # Get the date the node was created.
+    my $get_original_date = "SELECT modified FROM content WHERE name=? AND version='1'";
+    $sth = $dbh->prepare($get_original_date);
+    $sth->execute($_);
+
+    my @node_data = $sth->fetchrow_array;
+    my $date = substr($node_data[0], 0, 10); # strip off times
     
-    # We need to examine the latest content of each node.
-    
-    my $check_content = "SELECT text FROM content WHERE name=? AND version=?";
-    my $sth = $dbh->prepare($check_content);
-       
-    $sth->execute($name, get_latest_version($dbh, $name)) or die $check_content;
-          
-    my $content;
-            
-    while (my @row = $sth->fetchrow_array)
-    { 
-      $content = $row[0];
-    }
-    
-    unless ($self->include_redirects && $self->include_redirects == 1)
+    # Check the current content of the node - is it a redirect?
+    if ($self->is_redirect($_))
     {
-      # If the content shows the page to be a redirect, skip it. We're only interested
-      # in nodes with real content.                  
-      next if $content =~ /^\#REDIRECT/;
+      # If yes, include in stats if user wants redirects
+      if ($self->include_redirects)
+      {
+        if ($nodes{$date}) { $nodes{$date}++;   }
+        else               { $nodes{$date} = 1; }
+        
+        $node_count++;
+      }
+      else
+      {
+        # User doesn't want redirects, decrement count for day this
+        # now-redirecting node was created.
+        $nodes{$date}-- if $nodes{$date};
+      }
     }
-                   
-    my $date = substr($node_data[1], 0, 10); # strip off times
+    else
+    {
+      # Not a redirect, add it to the stats.
+      if ($nodes{$date}) { $nodes{$date}++;   }
+      else               { $nodes{$date} = 1; }
+      
+      $node_count++;
+    }
+  }  
 
-    # Count occurrences of each date.
+  $self->node_count($node_count);
 
-    if ($nodes{$date}) { $nodes{$date}++;   }
-    else               { $nodes{$date} = 1; }
-  }
+  \%nodes;
+}
 
-  return %nodes;
+sub is_redirect
+{
+  my ($self, $node) = @_;
+  my $dbh = $self->dbh;
+  
+  my $check_content = "SELECT text FROM content WHERE name=? AND version=?";
+  my $sth = $dbh->prepare($check_content);
+
+  my $version = $self->get_latest_version($node);
+  
+  $sth->execute($node, $version) or die $check_content;
+      
+  my @node_content = $sth->fetchrow_array;
+
+  return 1 if $node_content[0] =~ /^\#REDIRECT/;
 }
 
 sub make_graphs
 {
   my $self  = shift;
-  my %nodes = $self->retrieve_node_data;
+  my %nodes = %{$self->retrieve_node_data};
 
-  my $error_threshold = $self->error_threshold || 15;
-  
   # Our hash has keys of the form yyyy-mm-dd.
   # Chart::Strip requires time_t values, so get those and fill up a new hash.
   
@@ -145,30 +181,9 @@ sub make_graphs
   {
     my ($year, $month, $day) = split('-', $_);
 
-    # We want to include days with a disproportionally high count
-    # that may have been caused by vandals, spammers or plain old
-    # crazy robots.
-    
-    # First step, remember how many pages were made in the previous
-    # day of all the days this loop is examining. Initialize the
-    # variable if we haven't already.
-    $previous_day = $nodes{$_} unless $previous_day;
-
-    # The default threshold is 15 times more than the previous day.
-    # Of course, we have to include days when no nodes were created,
-    # otherwise we could get lots of false positives.
-    next if ($previous_day > 1) && ($nodes{$_} > ($previous_day * $error_threshold));
-
-    # If we got here the count was valid, so remember it for the 
-    # next pass through the loop.
-    $previous_day = $nodes{$_};
-
     # Total number of nodes.
     $count += $nodes{$_};
- 
-    # Store total.
-    $self->node_count($count);
-    
+     
     $month--;      # I hate POSIX.
     $year -= 1900; # No, really. I do.
 
@@ -190,6 +205,7 @@ sub make_graphs
     # at one point, you probably want that day to be included so as
     # not to have a huge spike at the beginning of your graph.
   }
+
 
   my $site_name          = $self->wiki_conf->site_name;
   my $width              = $self->graph_width        || 640;
@@ -237,9 +253,10 @@ sub make_graphs
 # Find out the most recent version number of a given node name.
 sub get_latest_version
 { 
-  my $dbh  = shift;
+  my $self = shift;
   my $name = shift;
-      
+  my $dbh  = $self->dbh;
+  
   my $get_versions = "SELECT version FROM content WHERE name=?";
            
   my $sth = $dbh->prepare($get_versions);
@@ -324,12 +341,7 @@ six-digit hex colour, e.g. 6699CC. Defaults to 000000.
 
 =item * C<rate_points_colour> The colour of the points in the rate graph. Defaults to 000000.
 
-=item * C<include_redirects> Show nodes that are only redirects to other nodes. Defaults to 0.
-
-=item * C<error_threshold> To avoid including days when disproportionately large numbers of nodes
-were created (for whatever reason) and distorting the graph, a threshold of nodes per day is set,
-counted in multiples of the previous day's total, at which point the day in question will not be
-included if it breaches the figure. Defaults to 15.
+=item * C<include_redirects> Show nodes that are only redirects to other nodes. Defaults to undef.
 
 =back
 
--- t/basic.t
+++ t/basic.t
@@ -1,4 +1,4 @@
-use Test::More tests => 4;
+use Test::More tests => 6;
 
 #1
 use_ok(OpenGuides::Statistics);
@@ -6,7 +6,8 @@ use_ok(OpenGuides::Statistics);
 my $stats;
 my $status = 'ok';
 
-eval {
+eval
+{
   $stats = OpenGuides::Statistics->new(wiki_conf => 't/wiki.conf');
 };
 
@@ -15,24 +16,50 @@ $status = $@ if $@;
 #2
 ok($status eq 'ok', 'load example OpenGuides config file');
 
-eval {
+eval
+{
   $stats = OpenGuides::Statistics->new;
 };
 
 $status = $@ if $@;
 
 #3
-ok($status =~ /^No wiki configuration file specified/, "can't start up without config file");
+ok($status =~ /^Error: No wiki configuration file specified/, "can't start up without config file");
 
-eval {
+eval
+{
   $stats = OpenGuides::Statistics->new(
-                                       wiki_conf    => 't/wiki.conf',
-                                       graph_width  => 640,
-                                       graph_height => 'fred'
+                                        wiki_conf    => 't/wiki.conf',
+                                        graph_width  => 640,
+                                        graph_height => 'fred',
                                       );
 };
 
 $status = $@ if $@;
 
 #4
-ok($status =~ /^Graph height and width must be numbers/, "can't use a duff graph option");
+ok($status =~ /^Error: 'graph_height' must be a number/, "can't use a malformed graph option");
+
+eval {
+  $stats = OpenGuides::Statistics->new(
+                                        wiki_conf    => 't/wiki.conf',
+                                        include_redirects => 2,
+                                      );
+};
+
+$status = $@ if $@;
+
+#5
+ok($status =~ /^Error: 'include_redirects' must be set to '1' if you set it./, "can't use an illogical include_redirects option");
+
+eval {
+  $stats = OpenGuides::Statistics->new(
+                                        wiki_conf    => 't/wiki.conf',
+                                        total_colour => 'fred',
+                                      );
+};
+
+$status = $@ if $@;
+
+#6
+ok($status =~ /^Error: 'total_colour' must be a 6-digit hex value/, "can't use a malformed colour value");
